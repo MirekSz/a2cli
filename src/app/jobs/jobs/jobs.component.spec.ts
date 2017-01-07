ff769b37cbf5d0c83294f42f0d8928df9ca3fc47
MirekSz <m.szajowski@gmail.com>
2017-01-07 15:17:02 +0100

    testy

--- src/app/jobs/jobs/jobs.component.spec.ts
+++ src/app/jobs/jobs/jobs.component.spec.ts
@@ -1,28 +1,37 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {DebugElement} from '@angular/core';
 
-import { JobsComponent } from './jobs.component';
+import {JobsComponent} from './jobs.component';
 
 describe('JobsComponent', () => {
-  let component: JobsComponent;
-  let fixture: ComponentFixture<JobsComponent>;
+    let component: JobsComponent;
+    let fixture: ComponentFixture<JobsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ JobsComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [JobsComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(JobsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(JobsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create component', () => {
+        expect(component).toBeTruthy();
+    });
+    it('should create component2', () => {
+        component.name = 'Mirek';
+        fixture.detectChanges();
+        
+        let de = fixture.debugElement.query(By.css('p'));
+        let el = de.nativeElement;
+
+        expect(el.textContent).toContain('Mirek')
+    });
 });
