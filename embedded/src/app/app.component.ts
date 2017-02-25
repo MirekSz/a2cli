b9cb18b2c2c5f56d8f840b35f5e87c4ffcd699cb
MirekSz <m.szajowski@gmail.com>
2017-02-25 18:09:54 +0100

    embedded

---
+++ embedded/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import monthPrinter from './MonthPrinter';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  title = 'app works!';
+  public userData: any = {program: 'NEXT + VERTO', user: ''};
+  private month: any;
+  private days: any;
+  private numbers: any;
+
+  ngOnInit(): void {
+    this.numbers=[];
+    for(let i=0;i<1000;i++){
+      this.numbers.push(i)
+    }
+    debugger;
+    let item = localStorage.getItem('userData');
+    if (item) {
+      this.userData =  JSON.parse(item);
+    }
+  }
+
+  print() {
+    monthPrinter(this.userData, this.month, this.days);
+    localStorage.setItem('userData',JSON.stringify(this.userData));
+  }
+
+  monthChange(event) {
+    this.month = event.month;
+    this.days = event.days;
+  }
+}
